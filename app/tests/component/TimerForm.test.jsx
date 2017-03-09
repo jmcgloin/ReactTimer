30b04f2b04b43d216cc4e219ee00251ffd2ee17c
Jason <jason.mcgloin@gmail.com>
2017-03-08 22:23:15 -0500

    add timerform tests

--- app/tests/component/TimerForm.test.jsx
+++ app/tests/component/TimerForm.test.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const expect = require('expect');
-// const $ = require('jQuery');
+const $ = require('jquery');
 const TestUtils = require('react-addons-test-utils');
 
 const TimerForm = require('TimerForm');
@@ -11,4 +11,25 @@ describe('TimerForm', () => {
 		expect(TimerForm).toExist();
 	});
 	
+	it('should call onSetTimer if valid seconds are entered', () => {
+		let spy = expect.createSpy();
+		let timerForm = TestUtils.renderIntoDocument(<TimerForm onSetTimer={spy} />);
+		let $el = $(ReactDOM.findDOMNode(timerForm));
+
+		timerForm.refs.seconds.value = '109';
+		TestUtils.Simulate.submit($el.find('form')[0]);
+
+		expect(spy).toHaveBeenCalledWith(109);
+	});
+
+	it('should not call onSetTimer if invalid seconds are entered', () => {
+		let spy = expect.createSpy();
+		let timerForm = TestUtils.renderIntoDocument(<TimerForm onSetTimer={spy} />);
+		let $el = $(ReactDOM.findDOMNode(timerForm));
+
+		timerForm.refs.seconds.value = 'apple';
+		TestUtils.Simulate.submit($el.find('form')[0]);
+
+		expect(spy).toNotHaveBeenCalled();
+	});
 });
