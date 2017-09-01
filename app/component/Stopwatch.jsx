e6e48b6988c8846399630c54fc56382a211b6bc8
Jason <jason.mcgloin@gmail.com>
2017-09-01 00:54:49 -0400

    a

--- app/component/Stopwatch.jsx
+++ app/component/Stopwatch.jsx
@@ -1,10 +1,68 @@
 const React = require('react');
+const Clock = require('Clock');
+const Controls = require('Controls');
 
 const Stopwatch = React.createClass({
+	getInitialState: function () {
+		return {
+			count: 0,
+			swStatus: 'stopped'
+		};
+	},
+	componentDidUpdate: function(prevProps, prevState) {
+		if(this.state.swStatus !== prevState.swStatus) {
+			switch(this.state.swStatus) {
+				case 'started':
+					this.startSW();
+					break;
+				case 'stopped':
+					this.setState({count: 0});
+				case 'paused':
+					clearInterval(this.timer);
+					this.timer = undefined;
+					break;
+			}
+		}
+	},
+	componentWillUnmount: function() {
+		clearInterval(this.timer);
+		this.timer = undefined;
+	},
+	handleStatusChange: function(newStatus) {
+		this.setState ({
+			swStatus: newStatus
+		});
+	},
+	startSW: function () {
+		this.timer = setInterval( () => {
+			this.setState({
+				count: this.state.count + 1
+			});
+		}, 1000);
+	},
+	onSubmit: function () {
+		this.setState ({
+			swStatus: 'started'
+		})
+	},
 	render: function() {
+		let {count,swStatus} = this.state;
+		let renderControlArea = () => {
+			return swStatus !== 'stopped' ? 
+				<Controls timerStatus={swStatus} onStatusChange={this.handleStatusChange}/> : 
+				<div className='row'>
+					<div className='columns medium-4 medium-centered'>
+						<form ref='form' onSubmit={this.onSubmit} className='sw-form'>
+							<button className='button expanded'>Start The Stopwatch</button>
+						</form>
+					</div>
+				</div>
+		};
 		return (
 			<div>
-				Stopwatch.jsx
+				<h1 className="page-title">Stopwatch App{swStatus}</h1>
+				<Clock totalSeconds={count}/>
+				{renderControlArea()}
 			</div>
 		);
 	}
